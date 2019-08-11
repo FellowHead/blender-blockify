1d3613a0c59fdc2428c31134a75a07acbddcea7f
FellowHead <thefellowhead@gmail.com>
2019-08-11 17:28:37 +0200

    introduced a way to refresh Blender's interface during processing

--- op.py
+++ op.py
@@ -11,65 +11,100 @@ class BlockifyOperator(bpy.types.Operator):
     bl_label = "Run object blockifier"
     bl_description = "Turn 'em into bloccs"
 
+    _timer = None
+    grid = None
+    valid_objects = None
+    obj = 0
+    frame = 0
+    dg = None
+    coll = None
+
     def execute(self, context):
+        self.grid = None
+
+        if Blockify.COLLECTION_NAME not in bpy.data.collections:
+            self.coll = bpy.data.collections.new(Blockify.COLLECTION_NAME)
+            context.scene.collection.children.link(self.coll)
+        else:
+            self.coll = bpy.data.collections[Blockify.COLLECTION_NAME]
+
+        self.valid_objects = []
+
+        for obj in context.scene.objects:
+            blk_obj = obj.blockify
+            if blk_obj.enabled and self.coll not in obj.users_collection:
+                self.valid_objects.append(obj)
+
+        self.frame = context.scene.blockify.frame_start - 1
+        self.obj = len(self.valid_objects)
+
         # clear old meshes
 
         rm = []
         for mesh in bpy.data.meshes:
-            if mesh.users == 0:  # maybe only perform on my meshes
+            if mesh.users == 0:  # TODO: only perform on my meshes?
                 rm.append(mesh)
         for mesh in rm:
             bpy.data.meshes.remove(mesh)
 
-        # real business
+        wm = context.window_manager
+        self._timer = wm.event_timer_add(time_step=0.5, window=context.window)
+        wm.modal_handler_add(self)
 
-        if Blockify.COLLECTION_NAME not in bpy.data.collections:
-            print("TIME FOR A FUCKING CRUSADE COLLECTION")
-            coll = bpy.data.collections.new(Blockify.COLLECTION_NAME)
-            context.scene.collection.children.link(coll)  # inline?
-        else:
-            coll = bpy.data.collections[Blockify.COLLECTION_NAME]
+        return {'RUNNING_MODAL'}
 
-        blk_glo = context.scene.blockify
+    def modal(self, context, event):
+        if event.type in {'RIGHTMOUSE', 'ESC'}:
+            self.cancel(context)
+            return {'FINISHED'}
 
-        i = blk_glo.frame_start
-        while i <= blk_glo.frame_end:
-            context.scene.frame_set(i)
-            dg = context.evaluated_depsgraph_get()
-            for obj in context.scene.objects:
-                blk_obj = obj.blockify
-                if blk_obj.enabled and coll not in obj.users_collection:
-                    print("BLOCKBLOCK " + str(i) + " " + obj.name)
+        if event.type == 'TIMER':
+            print("oof")
+            blk_glo = context.scene.blockify
 
-                    blk = blk_obj if blk_obj.override_block_size else blk_glo
-                    block_size = Vector(blk.block_size)
-                    div = blk.divide_by_vector
-                    precision = blk_obj.precision
+            if self.obj >= len(self.valid_objects):
+                self.obj = 0
+                self.frame = self.frame + 1
+                if self.frame > blk_glo.frame_end:
+                    print("damn son")
+                    self.cancel(context)
+                    return {'FINISHED'}
+                context.scene.frame_set(self.frame)
+                self.dg = context.evaluated_depsgraph_get()
 
-                    if div:
-                        block_size = Vector((
-                            1 / block_size.x,
-                            1 / block_size.y,
-                            1 / block_size.z
-                        ))
+            obj = self.valid_objects[self.obj]
+            blk_obj = obj.blockify
+            if self.grid is None:
+                blk = blk_obj if blk_obj.override_block_size else blk_glo
+                block_size = Vector(blk.block_size)
+                div = blk.divide_by_vector
+                precision = blk_obj.precision
 
-                    grid = Blockify.compute_grid(
-                        dg.objects[obj.name])
-                    # Blockify.write_grid_file("grid.fwd", grid)
-                    # grid = Blockify.read_grid_file("grid.fwd")
+                if div:
+                    block_size = Vector((
+                        1 / block_size.x,
+                        1 / block_size.y,
+                        1 / block_size.z
+                    ))
 
-                    name = "zzz_" + obj.name + "_" + str(i)
-                    if name in bpy.data.meshes:
-                        mesh = bpy.data.meshes[name]
-                    else:
-                        mesh = bpy.data.meshes.new(name)
-                    Blockify.create_mesh(grid, mesh)
+                self.grid = Blockify.compute_grid(
+                    self.dg.objects[obj.name])
+            else:
+                name = "zzz_" + obj.name + "_" + str(self.frame)
+                if name in bpy.data.meshes:
+                    mesh = bpy.data.meshes[name]
+                else:
+                    mesh = bpy.data.meshes.new(name)
+                Blockify.create_mesh(self.grid, mesh)
 
-                    name = "zzz_" + obj.name
-                    if name not in coll.objects:
-                        coll.objects.link(bpy.data.objects.new(name, mesh))
-            i = i + 1
-        # context.evaluated_depsgraph_get().update()
-        # blockify(blockSize=blockSize, precision=precision)
-        # self.report({'INFO'}, "Done current frame")
-        return {"FINISHED"}
+                name = "zzz_" + obj.name
+                if name not in self.coll.objects:
+                    self.coll.objects.link(bpy.data.objects.new(name, mesh))
+                self.grid = None
+                self.obj = self.obj + 1
+
+        return {'PASS_THROUGH'}
+
+    def cancel(self, context):
+        wm = context.window_manager
+        wm.event_timer_remove(self._timer)
